0d9f42da816727a5e867bfb9756f045ea0072b0e
SmartEggAnn <815177551@qq.com>
2016-12-11 13:21:48 +0800

    外观模式
    外观模式

---
+++ Classes/SceneManager.h
@@ -0,0 +1,17 @@
+#ifndef _SCENEMANAGER_H_
+#define _SCENEMANAGER_H_
+#include "HelpScene.h"
+#include "LoseScene.h"
+#include "WinScene.h"
+#include "TollgateScene.h"
+#include "cocos2d.h"
+USING_NS_CC;
+
+class SceneManager{
+public:
+	void helpScene(){ Director::getInstance()->replaceScene(TransitionCrossFade::create(1.0f, HelpScene::createScene())); }
+	void winScene(){ Director::getInstance()->replaceScene(WinScene::createScene()); }
+	void loseScene(){ Director::getInstance()->replaceScene(TransitionFade::create(1.0f, LoseScene::createScene())); }
+	void tollgateScene(){ Director::getInstance()->replaceScene(TransitionFade::create(1.0f, TollgateScene::createScene())); }
+};
+#endif
