4290636902d4780a819ca15b1921177809f36338
Scott <824260107@com>
2016-12-13 18:52:10 +0800

    test

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -24,7 +24,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
     if(!glview) {
-        glview = GLViewImpl::create("Tank");
+        glview = GLViewImpl::create("Running Tank");
 		glview->setFrameSize(1024, 700);
         director->setOpenGLView(glview);
     }
