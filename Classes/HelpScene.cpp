27230bfedcbfe59072069249941f366750acd2a6
SmartEggAnn <815177551@qq.com>
2016-12-13 17:42:40 +0800

    Update HelpScene.cpp

--- Classes/HelpScene.cpp
+++ Classes/HelpScene.cpp
@@ -21,8 +21,7 @@ void HelpScene::createLabel(string content, string font, int size, int width, in
 }
 
 bool HelpScene::init() {
-	if (!Layer::init())
-	{
+	if (!Layer::init()) {
 		return false;
 	}
 	Size visibleSize = Director::getInstance()->getVisibleSize();
@@ -70,15 +69,5 @@ bool HelpScene::init() {
 
 void HelpScene::menuCloseCallback(Ref* pSender)
 {
-/*#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
-	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.", "Alert");
-	return;
-#endif
-
-	Director::getInstance()->end();
-
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-	exit(0);
-#endif*/
 	Director::getInstance()->replaceScene(TransitionFade::create(1.0f,TollgateScene::createScene()));
 }
